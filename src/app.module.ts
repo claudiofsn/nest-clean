fe6158915cd1a390244545fc87dfc93c95d95f04
claudio.neto <claudio.neto@alcans.com.br>
2024-02-06 09:23:25 -0300

    Adicionando variaveis .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
 import { CustomerController } from './controllers/customer.controller'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from 'env'
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    validate: env => envSchema
+  })],
   controllers: [
     CustomerController
   ],
--- src/controllers/customer.controller.ts
+++ src/controllers/customer.controller.ts
@@ -1,16 +1,27 @@
-import { Body, ConflictException, Controller, HttpCode, Post } from "@nestjs/common";
+import { Body, ConflictException, Controller, HttpCode, Post, UsePipes } from "@nestjs/common";
+import { hash } from "bcryptjs";
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
 import { PrismaService } from "src/prisma/prisma.service";
+import { z } from "zod";
+
+const createAccountBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string()
+})
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
 
 @Controller('/customer')
 export class CustomerController {
-
     constructor(
         private prisma: PrismaService
     ) { }
 
     @Post()
     @HttpCode(201)
-    async create(@Body() { email, name, password }: { name: string; email: string; password: string }) {
+    @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+    async create(@Body() { email, name, password }: CreateAccountBodySchema) {
 
         const emailInUse = await this.prisma.user.findFirst({
             where: {
@@ -22,11 +33,13 @@ export class CustomerController {
             throw new ConflictException("User with same email address already exist")
         }
 
+        const hashedPassword = await hash(password, 8)
+
         await this.prisma.user.create({
             data: {
                 name,
                 email,
-                password
+                password: hashedPassword
             }
         })
     }
