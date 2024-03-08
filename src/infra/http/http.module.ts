8d93db9bf0dcb293f2f92c014f5e332ea2e60701
claudio.neto <claudio.neto@alcans.com.br>
2024-03-08 15:22:28 -0300

    parei na aula Comunicação entre camadas

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,7 +1,6 @@
 import { Module } from "@nestjs/common";
 import { CustomerController } from "./controllers/customer.controller";
 import { QuestionsController } from "./controllers/questions.controller";
-import { PrismaService } from "../database/prisma/prisma.service";
 import { DatabaseModule } from "../database/database.module";
 
 @Module({
